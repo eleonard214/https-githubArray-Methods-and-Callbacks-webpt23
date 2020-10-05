7627ed0d94eea3c6ea17f1665bf525733818e830
Erica Leonard <elynn1979@live.com>
2020-10-05 13:45:50 -0400

    Task 3 Completed

--- index.js
+++ index.js
@@ -27,21 +27,20 @@ for(let i = 0; i < team.length; i++){
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
-};
+function getFinals(data) {
+    return data.filter(element => (element.Stage === "Final"))
+}
+console.log("getFinals", getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+function getYears(cb, data) {
+    let years = []    
+    cb(data).map(item => years.push(item.Year))
+    return years
+}
 
-    /* code here */
-
-};
-
-getYears();
+console.log("getYears", getYears(getFinals, fifaData))
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
